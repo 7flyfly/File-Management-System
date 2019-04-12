7d39eb62ef4c1d55df16fbdda28af0ccd5678321
xinxindiandeng <1507791975@qq.com>
2019-04-12 13:50:36 +0800

    integratedQuery xuxin

--- management/src/main/java/com/file/management/controller/IntegratedQuery.java
+++ management/src/main/java/com/file/management/controller/IntegratedQuery.java
@@ -3,26 +3,43 @@ package com.file.management.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 
 /*
   综合查询
  */
 @Controller
+@RequestMapping("/IntegratedQuery")
 public class IntegratedQuery {
 
-
-
-    @RequestMapping("/AdvancedSearch")
-    public String AdvancedSearch(){
-        return "IntegratedQuery/AdvancedSearch";
+    @RequestMapping("/QueryHomePage")
+    public String integratedQuery(){
+        return "IntegratedQuery/ControlCenter";
     }
 
     @RequestMapping("/IntelligentRetrieval")
-    public String IntelligentRetrieval(){
+    public String intelligentRetrieval(){
         return "IntegratedQuery/IntelligentRetrieval";
     }
 
+    @RequestMapping("/AdvancedSearch")
+    public String advancedSearch(){
+        return "IntegratedQuery/AdvancedSearch";
+    }
+
+    @RequestMapping("/GetKeyWord")
+    @ResponseBody
+    public String getKeyWord(String keyword){
+        System.out.println("keyword:"+keyword);
+//        String keyword1 = request.getParameter("keyword");
+//        System.out.println("keyword:"+keyword1);
+        return keyword;
+    }
 
 
 
