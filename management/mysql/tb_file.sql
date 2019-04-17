5d503a7a1f5e56898a714510026e89e6a1d73638
7flyfly <1921430482@qq.com>
2019-04-17 09:54:48 +0800

    加入mysql文件

---
+++ management/mysql/tb_file.sql
@@ -0,0 +1,31 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : jdbc
+Source Server Version : 50528
+Source Host           : localhost:3306
+Source Database       : tb_file
+
+Target Server Type    : MYSQL
+Target Server Version : 50528
+File Encoding         : 65001
+
+Date: 2019-04-17 09:48:43
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `tb_users`
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_users`;
+CREATE TABLE `tb_users` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tb_users
+-- ----------------------------
+INSERT INTO `tb_users` VALUES ('admin', '123');
