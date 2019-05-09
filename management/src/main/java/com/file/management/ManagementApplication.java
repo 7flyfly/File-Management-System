1227ba4dea232f689d6b31eeca70472317df4340
yuyiaili <318934202@qq.com>
2019-05-09 16:24:03 +0800

    添加了定时注释

--- management/src/main/java/com/file/management/ManagementApplication.java
+++ management/src/main/java/com/file/management/ManagementApplication.java
@@ -2,8 +2,11 @@ package com.file.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+//定时任务
+@EnableScheduling
 public class ManagementApplication {
 
     public static void main(String[] args) {
