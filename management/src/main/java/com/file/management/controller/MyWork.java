e261eb7ad8ea2857fd22176e0405dd102cd10dc0
yuyiaili <318934202@qq.com>
2019-05-09 15:40:37 +0800

    上传了状态定时任务，在系统管理加入了状态和动作的管理，添加了我的工作页面

--- management/src/main/java/com/file/management/controller/MyWork.java
+++ management/src/main/java/com/file/management/controller/MyWork.java
@@ -1,7 +1,16 @@
 package com.file.management.controller;
 
+import com.file.management.dao.FileResposity;
+import com.file.management.pojo.ExpiredFile;
+import com.file.management.pojo.state.Message;
+import com.file.management.service.state.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
 
 
 /*
@@ -10,8 +19,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MyWork {
 
+    @Autowired
+    private MessageService messageService;
     @RequestMapping("/mywork")
-    public String myWork(){
+    public String myWork( Map<String,Object> map){
+        List<Message> lm = messageService.findAll();
+        int num = lm.size()-5;
+        for(int i=1;i<=5;i++){
+            Message m = messageService.findById(num+i);
+            if(m.getMessage()!=null){
+                String str = "mes0"+i;
+                map.put(str,m.getMessage());
+            }
+        }
         return "mywork";
     }
 
@@ -23,5 +43,30 @@ public class MyWork {
     @RequestMapping("/emps")
     public String emps(){
         return  "emps";
+    }
+
+    @RequestMapping("/register01")
+    public  String register01(){
+        return "register";
+    }
+
+    @RequestMapping("/MyWork/ExpiredFile")
+    public  String ExpiredFile(){
+        return "MyWork/ExpiredFile";
+    }
+
+    @RequestMapping("/MyWork/FileCount")
+    public  String FileCount(){
+        return "MyWork/FileCount";
+    }
+
+
+    @Autowired
+    private FileResposity fileResposity;
+    @RequestMapping(value = "/MyWork/MatureFile")
+    public String FileManagement(Model model){
+        List<ExpiredFile> list = fileResposity.findAll();
+        model.addAttribute("filelist",list);
+        return "/MyWork/MatureFile";
     }
 }
